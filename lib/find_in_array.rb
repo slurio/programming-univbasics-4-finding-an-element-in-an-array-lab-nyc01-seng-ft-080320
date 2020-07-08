351de3e19a60b2fef20199d71df84c648d0eff8f
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-08 03:15:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
-    puts array[count] == value_to_find
+    if array[count] == value_to_find
+      return count
+    end
     count += 1
   end
 end
 
-find_element_index([1,2,3,4,5], 2)
